8e0d08f6979f2e75019a7a8750d8023b029461b5
sgigli <simon.luciano@gigli.net>
2020-04-22 16:10:12 -0400

    Refactored with obj shorthand & destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=ec8092c5e8888945d0a198a0225e5999&query=${latitude},${longitude}&units=f`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + '. The current temperature is '
-                + response.body.current.temperature +
-                ' degrees. It feels like ' + response.body.current.feelslike + 
+            callback(undefined, body.current.weather_descriptions[0] + '. The current temperature is '
+                + body.current.temperature +
+                ' degrees. It feels like ' + body.current.feelslike + 
                 ' degrees.')
         }
     })
